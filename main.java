1054f9127168380b4853478b40948063cb985423
ArthurNeves1 <33200138+ArthurNeves1@users.noreply.github.com>
2017-11-26 13:38:10 -0300

    Update main.java

--- main.java
+++ main.java
@@ -3,20 +3,53 @@ package RedeSociall;
 public class main {
 
 	public static void main(String[] args) {
-		Cadastro ax = new Cadastro();
+		Cadastro Cadastro = new Cadastro();
+		
 		Usuario Arthur = new Usuario(1234,"arthurnbnb","arthurneves010301");
 		Usuario Isaac = new Usuario(5678,"isacasb","isacantonio");
+		Usuario Joao = new Usuario(3698,"Joaoskl","joaozim");
+		Usuario Caio = new Usuario(4875,"caiobar","cainho123");
+		Usuario Andre = new Usuario(9472,"Andregayzao","laranjinha1");
 		
-		ax.cadastrar(Arthur);
-		ax.cadastrar(Isaac);
-		ax.mostrarCadastrados();
 		
-		ax.logar(Arthur, "arthurnbn","arthurneves010301");
-		Sala x = new Sala();
-		x.fazerPostagem("Arthur é Lindo");
-		x.mostarFeed();
-		x.fazerPostagem("Lllllllllll");
-		x.mostarFeed();
+		Cadastro.cadastrar(Arthur);
+		Cadastro.cadastrar(Isaac);
+		Cadastro.cadastrar(Joao);
+		Cadastro.cadastrar(Caio);
+		Cadastro.cadastrar(Andre);
+		
+		Cadastro.mostrarCadastrados();
+		
+		Sala The_Walking_Dead = new Sala();
+		Sala Narcos = new Sala();
+		Sala Vikings = new Sala();
+		
+		The_Walking_Dead.entrarSala(Arthur);
+		The_Walking_Dead.entrarSala(Joao);
+		The_Walking_Dead.fazerPostagem(Arthur, "Uma otima serie",5);
+		The_Walking_Dead.fazerPostagem(Joao, "Serie razoavel",3);
+		The_Walking_Dead.mostarFeed(Joao);
+		
+		Narcos.entrarSala(Arthur);
+		Narcos.entrarSala(Joao);
+		
+		Vikings.entrarSala(Caio);
+		Vikings.entrarSala(Andre); 
+		
+		Isaac.logar("isacasb","isacantonio");
+		Isaac.enviarMensagem("Caioooo",Caio);
+
+		Caio.logar("caiobar","cainho123");
+		Caio.mostrarMensagensRecebidas();
+		Caio.enviarMensagem("Oi?", Isaac);
+		
+		Isaac.mostrarMensagensRecebidas();
+		Isaac.enviarMensagem("Passa a atividade", Caio);
+		
+		Caio.mostrarMensagensRecebidas();
+		
+		
+		
 	}
 
 }
