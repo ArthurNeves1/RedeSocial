9a6603abfd2257f092364664e422181b13f873aa
ArthurNeves1 <33200138+ArthurNeves1@users.noreply.github.com>
2017-11-26 13:31:33 -0300

    Update Sala.java

--- Sala.java
+++ Sala.java
@@ -5,22 +5,35 @@ import java.util.List;
 
 
 public class Sala {
-	
+	List <Usuario> participantes = new ArrayList<>();
 	List <String> feed = new ArrayList<>();
+	String nome;
+	String dataCriacao;
+	
 	public Sala() {
 		
 	}
-	
-	
-	public void fazerPostagem(String a) {
-		feed.add(a);
-		
+//	Adiciona um usuario a sala
+	public void entrarSala(Usuario x) {
+		participantes.add(x);
 	}
-	public void mostarFeed() {
-		for (String l : this.feed) {
-			System.out.println(l);
-		}
+//	Percorre a lista de participantes para olhar se ele esta na sala e so deixa ele fazer a postagem se estiver logado e
+//	tiver participando da sala
+	public void fazerPostagem(Usuario x,String a,int p) {
+		for (Usuario l : this.participantes) {
+			if(l.getIdUsuario() == x.getIdUsuario() && x.getLogin() == true) {
+				feed.add("Comentario: " + a + " Avaliação: " + Integer.toString(p) + " Usuario: " + x.getE_Mail());
+				System.out.println("Postagem realizada");
+			}
+		}	
 	}
-	
-	
+//	Percorre a lista de de postagem da sala printando as postagens
+	public void mostarFeed(Usuario x) {
+		for (Usuario l : this.participantes) {
+			if(l.getIdUsuario() == x.getIdUsuario()) { 
+				for (String f : this.feed) {
+					System.out.println(f);}
+			}
+		}	
+	}
 }
